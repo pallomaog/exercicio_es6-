21433dc8b2b3b7ca1b0563187c7e6c5e8dca7ac3
palloma g <90066965+pallomaog@users.noreply.github.com>
2023-11-13 10:33:12 -0300

    adição de arquivos do exercício de es6

---
+++ exercicio_es6/exercicio_es6_mod25-exercicio_es6/main.js
@@ -0,0 +1,25 @@
+const alunos = [
+    { nome: '', nota: 0 },
+    { nome: '', nota: 0 },
+    { nome: '', nota: 0 },
+    { nome: '', nota: 0 },
+    { nome: '', nota: 0 },
+    { nome: '', nota: 0 },
+    { nome: '', nota: 0 },
+]
+
+const alunosComNota = alunos.map(aluno => {
+    nomeAluno = prompt('Digite o nome do aluno')
+    notaAluno = prompt(`Digite a nota do aluno ${nomeAluno}`)
+
+    return {
+        name: nomeAluno,
+        nota: notaAluno
+    }
+}).filter(aluno => {
+    if (aluno.nota >= 6) {
+        return aluno
+    }
+})
+
+console.log(alunosComNota);
